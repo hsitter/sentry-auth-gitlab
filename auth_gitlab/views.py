2a735c2917063973df8c9ae44cb47b7711d71f82
Harald Sitter <sitter@kde.org>
2023-11-14 09:45:07 +0100

    allow configuring a required group

--- auth_gitlab/views.py
+++ auth_gitlab/views.py
@@ -1,10 +1,14 @@
 from sentry.auth.view import AuthView
 
 from .client import GitLabClient
-
+from .constants import GROUPS
 class FetchUser(AuthView):
     def handle(self, request, helper):
         with GitLabClient(helper.fetch_state('data')['access_token']) as client:
             user = client.get_user()
+
+            if GROUPS and not any(group in user['groups'] for group in GROUPS):
+                return helper.error(f'Only members of {GROUPS} are allowed to log in. If you are a Qt developer you can request access in a KDE sysadmin ticket — https://go.kde.org/systickets . Your groups are {user["groups"]}')
+
             helper.bind_state('user', user)
             return helper.next_step()
